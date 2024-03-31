086231d7885697ef1fac4011a3542278871d6389
tommya98 <tommya98@naver.com>
2024-03-31 21:56:22 +0900

    FEAT: Nav 컴포넌트 구조 구현

--- Front-End Developer Capstone/little-lemon/src/components/Nav.js
+++ Front-End Developer Capstone/little-lemon/src/components/Nav.js
@@ -1,5 +1,47 @@
 function Nav() {
-  return <nav>nav</nav>;
+  return (
+    <div>
+      <img src="/assets/Logo.svg" alt="little-lemon logo" />
+      <nav>
+        <ul>
+          {navMenu.map((item, idx) => {
+            return (
+              <li key={idx}>
+                <a href={item.link}>{item.name}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Nav;
+
+const navMenu = [
+  {
+    name: "Home",
+    link: "",
+  },
+  {
+    name: "About",
+    link: "",
+  },
+  {
+    name: "Menu",
+    link: "",
+  },
+  {
+    name: "Reservations",
+    link: "",
+  },
+  {
+    name: "Order Online",
+    link: "",
+  },
+  {
+    name: "Login",
+    link: "",
+  },
+];
