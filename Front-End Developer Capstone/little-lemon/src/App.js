974bf755bf1d285eae4962fd222268355a7e4fa1
tommya98 <tommya98@naver.com>
2024-03-31 21:44:43 +0900

    ADD: 초기 HTML 구조 작성

--- Front-End Developer Capstone/little-lemon/src/App.js
+++ Front-End Developer Capstone/little-lemon/src/App.js
@@ -1,5 +1,17 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ Front-End Developer Capstone/little-lemon/src/components/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+  return (
+    <>
+      <section>section1</section>
+      <section>section2</section>
+      <section>section3</section>
+    </>
+  );
+}
+
+export default Main;
