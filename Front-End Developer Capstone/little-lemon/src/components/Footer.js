5ac71d0860a5b70a85e4e29a367d46413f31b5da
tommya98 <tommya98@naver.com>
2024-03-31 22:04:57 +0900

    FEAT: Footer 컴포넌트 구조 구현

--- Front-End Developer Capstone/little-lemon/src/components/Footer.js
+++ Front-End Developer Capstone/little-lemon/src/components/Footer.js
@@ -1,5 +1,36 @@
 function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer>
+      <img src="/assets/Logo.svg" alt="little-lemon logo" />
+      <div>
+        <span>Doormat Nav igation</span>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+      </div>
+      <div>
+        <span>Contact</span>
+        <ul>
+          <li>Address</li>
+          <li>Phone number</li>
+          <li>Email</li>
+        </ul>
+      </div>
+      <div>
+        <span>Social Media Links</span>
+        <ul>
+          <li>Address</li>
+          <li>Phone number</li>
+          <li>Email</li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
